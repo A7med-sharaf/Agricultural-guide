0f74d4d4ab70658e158968f27d3011ba821a177c
A7med-sharaf <a9986939@gmail.com>
2024-07-20 19:45:35 +0300

    vertion 1

---
+++ translate.js
@@ -0,0 +1,194 @@
+
+let lang = {
+
+
+    ar: {
+        home :"  الرئيسية",
+        news:" الاخبار",
+        status: " الولايات",
+        clients: "العملاء",
+        ContactUs: "اتصل بنا",
+        arabic: "العربية",
+        english: "الانجليزية",
+        // footer 
+        footer1:" رؤيتنا",
+        footer2:"الطاقات والابتكار ومنطقتنا هي الاستقرار والعدالة والرخاء",
+        footer3:"رسالتنا",
+        footer4:"بالشغف والعزيمة والعمل نبتكر وننتج المعرفة ونقدم المشورة ونبني الإجماع ونواكب المنطقة العربية على طريق أجندة 2030. يداً بيد نبني غداً مشرقاً لكل إنسان.",
+
+
+        
+        investor1:"الاستثمار الزراعي",
+        investor2:"- الاستثمار في القطاع الزراعي عالميا قد يوفر للمستثمرين عوائد أفضل من الاستثمارات التقليدية كالسندات والأسهم وذلك بفضل زيادة الطلب على الغذاء عالميا.وتشير التوقعات إلى أن أسعار المنتجات الزراعية ستستمر في الارتفاع مما يفتح المجال لتحقيق عوائد استثمارية طويلة الأجل للمستثمرين في هذا القطاع.وسيستمر هذا الازدهار الزراعي في الارتفاع وهو ليس ظاهرة لحظية أو متقلبة مما يعني استمرار ظاهرة ارتفاع الأسعار التي تشهدها الأسواق حاليا.ستشهد الحبوب والأرز والزيوت النباتية والسكر واللحوم ومنتجات الألبان نموا مستمرا مما يمنح المستثمرين إمكانية تحقيق عوائد جيدة والاستثمار في القطاعات الزراعية قد يجلب للمستثمرين عوائد تختلف عن العوائد المحققة من الاستثمار في الأصول التقليدية كالأسهم والسندات.ثانيا: تنوع المناخ في مختلف أنحاء الجمهورية.ثالثا: توفر المواد الخام بكميات كبيرة للمشاريع في الصناعات الغذائية.رابعا: اليمن لديها قوة عاملة تتميز بانخفاض تكلفتها وقدرتها التنافسية ومهاراتها العالية.خامسا: هطول الأمطار بكميات كبيرة في بعض محافظات الجمهورية.سادساً: تتمتع اليمن بموقع استراتيجي يطل مباشرة على خطوط الملاحة البحرية الدولية بين أوروبا وآسيا، كما تعتبر المدخل الرئيسي لشرق أفريقيا.",
+        // Automation services
+        auto1:"خدمات الأتمتة ",
+        auto2:"الابتكار هو أكثر أهمية في الزراعة الحديثة من أي وقت مضى. تواجه الصناعة ككل تحديات هائلة: من ارتفاع تكاليف الإمدادات ونقص العمالة إلى التغيرات في تفضيلات المستهلكين للشفافية والاستدامة. هناك اعتراف متزايد من شركات الزراعة بأن هناك حاجة إلى حلول فورية لهذه التحديات. لحسن الحظ، تكنولوجيا الزراعة، والمعروفة أيضًا باسم agtech، موجودة هنا. يشير هذا المفهوم الجديد إلى استخدام التكنولوجيا في الزراعة والممارسات الزراعية لزيادة الكفاءة والإنتاجية والاستدامة في إنتاج الغذاء. ويشمل عدة أنواع مختلفة من التقنيات، مثل الزراعة الدقيقة، والري الذكي، والتكنولوجيا الحيوية، والأتمتة. بالإضافة إلى ذلك، هناك تقدم تكنولوجي كبير في مجالات مثل الزراعة الرأسية الداخلية، وتكنولوجيا الثروة الحيوانية، وممارسات الدفيئة الحديثة، والذكاء الاصطناعي، والبلوك تشين، والتي سنستكشفها بمزيد من التفصيل في هذه المقالة.",
+        auto3:"احصل على الخدمات",
+
+        // Why you should chooes Us
+        why1:"لماذا يجب عليك اختيارنا...؟",
+        why2:"الاستثمار هو وسيلة فعّالة لجعل أموالك تعمل لصالحك وبناء الثروة. يعتبر الاحتفاظ بالنقود وحسابات التوفير المصرفية من الاستراتيجيات الآمنة، ولكن استثمار أموالك يسمح لها بالنمو في القيمة بمرور الوقت مع الاستفادة من الفائدة المركبة والنمو الطويل الأجل. سواء كنت تستثمر في الأسهم أو السندات أو صناديق الاستثمار المشترك أو الخيارات أو العقود الآجلة أو المعادن الثمينة أو العقارات أو الشركات الصغيرة، فإن الاستثمار مهم لتوليد الدخل المستقبلي وزيادة القيمة والأسهم وبناء الثروة.",
+        why3:"لدينا خبرة كافية لأكثر من 10 سنوات",
+        why4:"توفير أحدث الآليات الحديثة",
+        why5:"توفير المهندسين والخبراء",
+        why6:"موقعنا الجغرافي مميز حيث يقع على خط التجار البحريين والبريين",
+        // Our two clients
+        clients1: " شركات الاسمدة والمبيدات",
+        clients2: "جامعة كردفان ",
+        clients3: "الخرطوم  المعمورة",
+        clients4: "094544221",
+        clients5: '"إنتاج الأسمدة"',
+        clients6: "أوبار1",
+        clients7: "  الخرطوم المعمورة",
+        clients8: "094544221",
+        clients9: '" إنتاج الأسمدة "',
+        clients10: "مركز تغذية دولي",
+        clients11: "094544221",
+        clients12: "انتاج الأسمدة ",
+        clients13: " O.BASF ",
+        clients14: " الخرطوم المعمورة ",
+        clients15: "094544221",
+        clients16: '"إنتاج الأسمدة"',
+        
+
+
+
+        //  section carousel
+        carousel1: "دليل زراعي",
+        carouse2: "منتجات عالية الأداء تحافظ على مساحات العشب الكبيرة جميلة وصحية",
+        carouse3: "اتصل بنا ",
+        carouse4: "دليل زراعي ",
+        carouse5: "السودان، سلة خبز العالم، لديه أكبر مساحة من الأراضي الصالحة للزراعة، 200 مليون فدان، منها 64 مليون فدان مزروعة.",
+        carouse6: "دليل زراعي",
+        carouse7: "ومن أهم المنتجات الزراعية المصدرة القطن والصمغ العربي والسمسم والفول السوداني والفواكه والخضروات، وتعتبر الصين والمملكة العربية السعودية والإمارات العربية المتحدة والهند ومصر الوجهة الرئيسية للصادرات السودانية.",
+        carouse8: "اتصل بنا",
+
+
+         // footer
+         footer11: "حقوق النشر",
+         footer22: "(2024) دليل الزراعة السوداني",
+
+
+
+
+
+
+
+
+    },
+    en: {
+        home : "home",
+        news:" news",
+        status: "status  ",
+        clients: "clients",
+        ContactUs: "Contact Us",
+        english:"English",
+        arabic: "Arabic",
+        // footer
+        footer1:" Our vision",
+        footer2:"Energies and innovation, and our region is stability, justice and prosperity",
+        footer3:"Our message",
+        footer4:"With passion, determination, and work: we innovate, produce knowledge, provide advice, build consensus, and keep pace with the Arab region on the path of the 2030 Agenda. Hand in hand, we build a bright tomorrow for every human being.",
+        investor1:"Agricultural investment",
+        investor2:"in the agricultural sector globally may provide investors with better returns than traditional investments such as bonds and stocks, thanks to the increased demand for food globally. Expectations indicate that the prices of agricultural products will continue to rise, which opens the way for achieving long-term investment returns for investors in this sector. This agricultural prosperity will continue to rise, and it is not an instantaneous or fluctuating phenomenon, which means the continuation of the phenomenon of rising prices that the markets are currently witnessing.Grains, rice, vegetable oils, sugar, meat, and dairy products will witness continuous growth, giving investors the potential to achieve good returns, and investing in agricultural sectors may bring investors returns that differ from the returns achieved from investing in traditional assets such as stocks and bonds.Second: The climate varies throughout the republic.     Third: Availability of raw materials in large quantities for projects in the food industries.Fourth: Yemen has a workforce distinguished by its low cost, competitiveness, and high skillFifth: Rainfall in large quantities in some governorates of the Republic.Sixth: Yemen enjoys a strategic location directly overlooking the international maritime shipping lines between Europe and Asia. It is also considered the main entrance to the eastern African",
+
+        // Automation services
+        auto1:"Automation services",
+        auto2:"Innovation is more important in modern agriculture than ever before. The industry as a whole is facing huge challenges: from rising costs of supplies and labor shortages to changes in consumer preferences for transparency and sustainability. There is increasing recognition from agriculture corporations that immediate solutions are needed for these challenges. Thankfully agriculture technology, also known as agtech is here This new concept refers to the use of technology in farming and agricultural practices to increase efficiency, productivity, and sustainability in food production. It includes several different types of technologies, such as precision agriculture, smart irrigation, biotechnology, and automation. Additionally, there are significant technological advancements in areas like indoor vertical farming, livestock technology, modern greenhouse practices, artificial intelligence, and blockchain, which we will explore further in this article,",
+        auto3:"Get the Sevvices",
+
+        // Why you should chooes Us
+        why1:" Why you should chooes Us...?",
+        why2:"Investing is an effective way to have your money work for you and build wealth. Holding cash and bank savings accounts are considered safe strategies, but investing yourmoney allows it to grow in value over time with the benefit of compounding and long-term growth Whether you invest in stocks, bonds, mutual funds, options, futures, precious metals, real estate, or small businesses, investing is important to generate future income, increase value and equity, and build wealth..",
+        why3:"We have sufficient experience for more than 10 years",
+        why4:"Providing the latest modern mechanisms",
+        why5:"Providing engineers and experts",
+        why6:"Our geographic location is distinctive, as it is located in the line of maritime and land merchants",
+          // Our two clients
+          clients1: "Fertilizer and pesticide companies",
+          clients2: "Yara International",
+          clients3: "Khartoum  Mamoura",
+          clients4: "094544221",
+          clients5: '"Fertilizer production"',
+          clients6: "opar1",
+          clients7: "hartoum  Maamoura",
+          clients8: "094544221",
+          clients9: '" Fertilizer production "',
+          clients10: "center feed international",
+          clients11: "094544221",
+          clients12: "fertilizer production ",
+          clients13: " O.BASF ",
+          clients14: " Khartoum Khartoum Maamoura ",
+          clients15: "094544221",
+          clients16: '" Fertilizer production "',
+
+          //  section carousel
+        carousel1: "Agricultural guide",
+        carouse2: " High-preforming products that keep large turf areas beautiful and healthy-while saving you time, water and energy",
+        carouse3: "Contact Us",
+        carouse4: "Agricultural guid",
+        carouse5: "Sudan, the world's breadbasket, has the most arable land, 200 million acres, of which 64 are cultivated",
+        carouse6: "Agricultural guide",
+        carouse7: "The most important exported agricultural products are cotton, gum arabic, sesame, peanuts, fruits and vegetables. The main destination for Sudanese exports is China, Saudi Arabia, the United Arab Emirates, India and Egypt",
+        carouse8: "Contact Us",
+
+
+         
+        // footer
+        footer11: "Copyright",
+        footer22: "2024; sudan Agricultural guide",
+        
+
+        
+    }
+}
+
+
+
+let selector = document.getElementById('language-selector');
+
+// استرجاع اللغة المخزنة في localStorage عند تحميل الصفحة
+document.addEventListener('DOMContentLoaded', (event) => {
+    let storedLanguage = localStorage.getItem('selectedLanguage');
+    if (storedLanguage) {
+        selector.value = storedLanguage;
+        updateLanguage();
+    } else {
+        updateLanguage(); // لضبط اللغة الافتراضية عند أول تحميل
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+selector.onchange = updateLanguage;
+function updateLanguage(){
+  let language = selector.options[selector.selectedIndex].value;
+  let nodes = document.querySelectorAll('[data-lang]');
+
+    let i = nodes.length;
+
+    while(i--) {
+        let key = nodes[i].getAttribute('data-lang');
+        nodes[i].innerHTML = lang[language][key];
+    }
+
+
+    if(language === "en"){
+        document.dir = "ltr";
+    }else{
+        document.dir = 'rtl';
+    }
+    
+    
+
+    localStorage.setItem('selectedLanguage', language);
+}
+
